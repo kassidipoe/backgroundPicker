b40c1395b534e018805fdee031aee498109d8ca1
Kassidi Poe <kassidipoe@gmail.com>
2024-04-30 13:53:32 -0400

    first commit

---
+++ backgroundPicker.js
@@ -0,0 +1,45 @@
+document.getElementById('purple').onclick = purpleParty
+document.getElementById('green').onclick = greenParty
+document.getElementById('cyan').onclick = cyanParty
+document.getElementById('red').onclick = redParty
+document.getElementById('yellow').onclick = yellowParty
+document.getElementById('blue').onclick = blueParty
+document.getElementById('white').onclick = whiteParty
+document.getElementById('black').onclick = blackParty
+
+
+
+function purpleParty() {
+	document.querySelector('body').style.backgroundColor = 'magenta'
+	document.querySelector('body').style.color = 'black'
+}
+
+function greenParty() {
+	document.querySelector('body').style.backgroundColor = '#0f0'
+	document.querySelector('body').style.color = 'black'
+}
+
+function cyanParty() {
+	document.querySelector('body').style.backgroundColor = 'cyan'
+	document.querySelector('body').style.color = 'black' 
+}
+function redParty() {
+	document.querySelector('body').style.backgroundColor = 'red'
+	document.querySelector('body').style.color = 'black'
+}
+function yellowParty() {
+	document.querySelector('body').style.backgroundColor = 'yellow'
+	document.querySelector('body').style.color = 'black'
+}
+function blueParty() {
+	document.querySelector('body').style.backgroundColor = 'blue'
+	document.querySelector('body').style.color = 'black'
+}
+function whiteParty() {
+	document.querySelector('body').style.backgroundColor = 'white'
+	document.querySelector('body').style.color = 'black'
+}
+function blackParty() {
+	document.querySelector('body').style.backgroundColor = 'black'
+	document.querySelector('body').style.color = 'white'
+}
